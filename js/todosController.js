22bde52529ae322ee2908e3d7ab24767da5997f1
john_lee <jjlee099@gmail.com>
2017-03-29 12:41:06 -0700

    adds scope branch using $scope

--- js/todosController.js
+++ js/todosController.js
@@ -4,35 +4,34 @@
   angular.module('todosApp')
     .controller('TodosController', TodosController)
 
-    function TodosController() {
-      var self = this
-      self.allTodos = [
+    function TodosController($scope) {
+      $scope.allTodos = [
       	{title: 'Take out trash', importance: 1, completed:false},
       	{title: 'Finish WDI', importance: 2, completed:false},
       	{title: 'Finish todos app', importance: 3, completed:false},
       	{title: 'Finish bootsy app, baby', importance: 5, completed:true},
       ]
-      self.newTodo = {
+      $scope.newTodo = {
       	title: '',
       	importance: 0, // 1 - 5
       	completed: false
       }
-      self.addTodo = addTodo
+      $scope.addTodo = addTodo
       function addTodo() {
-        self.allTodos.push({
-          title: self.newTodo.title,
-          importance: self.newTodo.importance,
-          completed: self.newTodo.completed
+        $scope.allTodos.push({
+          title: $scope.newTodo.title,
+          importance: $scope.newTodo.importance,
+          completed: $scope.newTodo.completed
         })
-        self.newTodo = {
+        $scope.newTodo = {
           title: '',
           importance: 0,
           completed: false
         }
       }
-      self.getCompleted = getCompleted
+      $scope.getCompleted = getCompleted
       function getCompleted() {
-      	return self.allTodos.filter(function(todo){ return todo.completed })
+      	return $scope.allTodos.filter(function(todo){ return todo.completed })
       }
 
      }
