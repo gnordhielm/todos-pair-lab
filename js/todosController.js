b514d6e117388d84b4c3ea8551d0b25f64045f61
Gus Nordhielm <gnordhielm@gmail.com>
2017-03-29 12:01:12 -0700

    display todos

--- js/todosController.js
+++ js/todosController.js
@@ -5,6 +5,18 @@
     .controller('TodosController', TodosController)
 
     function TodosController() {
-      
+      var self = this
+      self.allTodos = [
+      	{title: 'Take out trash', importance: 1, completed:false},
+      	{title: 'Finish WDI', importance: 2, completed:false},
+      	{title: 'Finish todos app', importance: 3, completed:false},
+      	{title: 'Finish bootsy app, baby', importance: 5, completed:true},
+      ]
+      self.newTodo = {
+      	title: '',
+      	importance: 0, // 1 - 5
+      	completed: false
+      }
+
     }
 }())
